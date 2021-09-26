ebfaed13db8547899ce2d80c73f3bb4ed2c9da9a
William <w3ye@pm.me>
2021-09-26 14:36:44 -0400

    refactor

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,9 +4,20 @@ const cookieParser = require("cookie-parser");
 const { verify } = require("jsonwebtoken");
 const { hash, compare } = require("bcryptjs");
 const { route } = require(".");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} = require("../tokens");
 const router = express.Router();
 
-module.exports = ({ getUsers, getUserByEmail, addUser }) => {
+module.exports = ({
+  getUsers,
+  getUserByEmail,
+  addUser,
+  updateRefreshToken,
+}) => {
   /* GET users listing. */
   router.get("/", (req, res) => {
     getUsers()
@@ -20,21 +31,22 @@ module.exports = ({ getUsers, getUserByEmail, addUser }) => {
 
   router.post("/", async (req, res) => {
     const { first_name, last_name, email, password } = req.body;
-    const user = await getUserByEmail(email);
-    if (user) {
-      res.json({
-        message: "User with this email already exists",
-      });
-    } else {
-      const hashedPassword = await hash(password, 10);
-      const newUser = await addUser(
-        first_name,
-        last_name,
-        email,
-        hashedPassword
-      );
-      if (!newUser) res.json({ error: err.message });
-      res.json(newUser);
+    try {
+      const user = await getUserByEmail(email);
+      if (user) throw new Error("User already exists");
+      else {
+        const hashedPassword = await hash(password, 10);
+        const newUser = await addUser(
+          first_name,
+          last_name,
+          email,
+          hashedPassword
+        );
+        if (!newUser) res.json({ error: err.message });
+        res.json(newUser);
+      }
+    } catch (err) {
+      res.json({ error: err.message });
     }
   });
 
@@ -46,8 +58,11 @@ module.exports = ({ getUsers, getUserByEmail, addUser }) => {
       const valid = await compare(password, user.password);
       if (!valid) throw new Error("Incorrect Password");
       // refresh and access token
-      const accessToken = ;
-      const refreshToken = ;
+      const accessToken = createAccessToken(user.id);
+      const refreshToken = createRefreshToken(user.id);
+      updateRefreshToken(user.id, refreshToken);
+      sendRefreshToken(res, refreshToken);
+      sendAccessToken(req, res, accessToken);
     } catch (err) {
       res.json({ errror: err.message });
     }
