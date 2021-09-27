9b3afaee86a8747b741b284919eed72b73d201bd
Juliet <li_juliet@hotmail.com>
2021-09-27 11:25:24 -0400

    to merge App.js

---
+++ client/src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import Dashboard from "../Dashboard/Dashboard";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import Preferences from "../Preferences/Preferences";
+import Topbar from "../home/Topbar";
+
+function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    // return <Login setToken={setToken} />;
+    // return <Register />
+    return <Topbar />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
