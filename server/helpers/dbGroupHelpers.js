7ff199a542722de41d40946a778cee9bd5f9dac7
William <w3ye@pm.me>
2021-09-28 00:36:01 -0400

    implement show all the groups a user belongs

--- server/helpers/dbGroupHelpers.js
+++ server/helpers/dbGroupHelpers.js
@@ -9,7 +9,25 @@ module.exports = (db) => {
       .catch((err) => err);
   };
 
+  const getUserGroups = (id) => {
+    const query = {
+      text: `
+        SELECT group_members.id, user_id, group_id, title
+        FROM group_members  
+        JOIN users ON group_members.user_id = users.id 
+        JOIN groups ON group_members.group_id = groups.id 
+        WHERE users.id = $1;
+      `,
+      values: [id],
+    };
+    return db
+      .query(query)
+      .then((result) => result.rows)
+      .catch((err) => err);
+  };
+
   return {
     getGroups,
+    getUserGroups,
   };
 };
--- server/routes/group.js
+++ server/routes/group.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = ({ getGroups }) => {
+module.exports = ({ getGroups, getUserGroups }) => {
   router.get("/", (req, res) => {
     getGroups()
       .then((groups) => res.json(groups))
       .catch((err) => res.json({ error: err.message }));
+  });
+
+  router.get("/:user_id", (req, res) => {
+    const userId = req.params.user_id;
+    getUserGroups(userId)
+      .then((groups) => {
+        res.json(groups);
+      })
+      .catch((err) => {
+        res.json({ error: err.message });
+      });
   });
 
   return router;
