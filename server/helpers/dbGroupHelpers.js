b8cb9cb602c24d0ce4a5271697e9879c7430c260
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-09-29 15:50:00 -0400

    remove userId in the select statement

--- server/helpers/dbGroupHelpers.js
+++ server/helpers/dbGroupHelpers.js
@@ -12,7 +12,10 @@ module.exports = (db) => {
   const getUserGroups = (id) => {
     const query = {
       text: `
-        SELECT group_members.id, user_id, group_id, title
+        SELECT 
+        group_members.id, 
+        group_id, 
+        title
         FROM group_members  
         JOIN users ON group_members.user_id = users.id 
         JOIN groups ON group_members.group_id = groups.id 
