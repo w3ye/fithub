b0ffa5df252b9f1733c0ae8520f8a1b7e98dc21f
Juliet <li_juliet@hotmail.com>
2021-09-29 13:47:34 -0400

    add filter to 12 response

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import Topbar from "../topbar/Topbar";
-import Leftbar from "./LeftPanel/Leftbar";
-import Center from "./Center/Center";
-import CenterWorkouts from "./Center/CenterWorkouts";
-import CenterFriends from "./Center/CenterFriends";
-import Rightbar from "./RightPanel/Rightbar";
-import RightbarWorkouts from "./RightPanel/RightbarWorkouts";
-import RightbarFriends from "./RightPanel/RightbarFriends";
-import "./index.scss";
+import Topbar from '../topbar/Topbar'
+import Leftbar from './LeftPanel/Leftbar'
+import Center from './Center/Center'
+import CenterWorkouts from './Center/CenterWorkouts'
+import CenterFriends from './Center/CenterFriends'
+import Rightbar from './RightPanel/Rightbar'
+import RightbarWorkouts from './RightPanel/RightbarWorkouts'
+import RightbarFriends from './RightPanel/RightbarFriends'
+import './index.scss'
 
-export default function Home(props) {
-  const { setMain } = props;
-  const [panels, setPanels] = useState("home");
-  const [workout, setWorkout] = useState([]);
+export default function Home (props) {
+  const { setMain } = props
+  const [panels, setPanels] = useState('home')
+  const [workout, setWorkout] = useState([])
 
-  const onAdd = (exercise) => {
-    setWorkout([...workout, { ...exercise, set: 1, reps: 10 }]);
-  };
+  const onAdd = exercise => {
+    setWorkout([...workout, { ...exercise, set: '1', reps: '10' }])
+  }
   return (
     <>
       <Topbar setMain={setMain} />
-      <div className="homeContainer">
+      <div className='homeContainer'>
         <Leftbar setPanels={setPanels} />
-        {panels === "home" && <Center onAdd={onAdd} />}
-        {panels === "home" && (
+        {panels === 'home' && <Center onAdd={onAdd} />}
+        {panels === 'home' && (
           <Rightbar workout={workout} setWorkout={setWorkout} />
         )}
-        {panels === "workouts" && <CenterWorkouts />}
-        {panels === "workouts" && <RightbarWorkouts />}
-        {panels === "friends" && <CenterFriends />}
-        {panels === "friends" && <RightbarFriends />}
+        {panels === 'workouts' && <CenterWorkouts />}
+        {panels === 'workouts' && <RightbarWorkouts />}
+        {panels === 'friends' && <CenterFriends />}
+        {panels === 'friends' && <RightbarFriends />}
       </div>
     </>
-  );
+  )
 }
