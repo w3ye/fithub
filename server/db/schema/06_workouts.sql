a859abec5b220005f9221e0062e93223258b4412
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-09-29 16:49:28 -0400

    rename workouts file

--- server/db/schema/06_workouts.sql
+++ server/db/schema/06_workouts.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS workout CASCADE;
-CREATE TABLE workout(
+DROP TABLE IF EXISTS workouts CASCADE;
+CREATE TABLE workouts(
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  group_id INTEGER REFERENCES groups(id),
+  group_id integer [],
   exercise jsonb
 );
