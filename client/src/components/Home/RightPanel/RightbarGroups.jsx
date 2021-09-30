2a59b95036f615a40603ab5e16e74e6b3f6b4231
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-09-30 17:31:16 -0400

    give max-width to left-bar and no right group panel showing if none selected

--- client/src/components/Home/RightPanel/RightbarGroups.jsx
+++ client/src/components/Home/RightPanel/RightbarGroups.jsx
@@ -3,13 +3,16 @@ import "./rightbar.scss";
 export default function RightbarGroups(props) {
   const { group } = props;
   console.log(group);
+
+  if (!group.title) {
+    return <></>;
+  }
   return (
     <div className="rightbar container">
       <h2>{group.title}</h2>
-      <h5>Add New Member</h5>
-      <input type="email" placeholder="User Email" />
-      <button>Add</button>
-      <ul></ul>
+      {group.title && <h5>Add New Member</h5>}
+      {group.title && <input type="email" placeholder="User Email" />}
+      {group.title && <button>Add</button>}
     </div>
   );
 }
