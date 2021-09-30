c74f0573091ffbef06bc5a05d8e50e150b4c59ab
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-09-30 16:19:50 -0400

    use variables instead of deconstruction

--- server/routes/group.js
+++ server/routes/group.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = ({
-  getGroups,
-  getUserGroups,
-  addUserToGroup,
-  checkUserInGroup,
-}) => {
+module.exports = (db) => {
   router.get("/", (req, res) => {
-    getGroups()
+    db.getGroups()
       .then((groups) => res.json(groups))
       .catch((err) => res.json({ error: err.message }));
   });
 
   router.get("/:user_id", (req, res) => {
     const userId = req.params.user_id;
-    getUserGroups(userId)
+    db.getUserGroups(userId)
       .then((groups) => {
         res.json(groups);
       })
@@ -26,10 +21,10 @@ module.exports = ({
 
   router.post("/add_group/:group_id/:user_id", (req, res) => {
     const { group_id, user_id } = req.params;
-    checkUserInGroup(group_id, user_id)
+    db.checkUserInGroup(group_id, user_id)
       .then((exists) => {
         if (!exists) {
-          addUserToGroup(group_id, user_id)
+          db.addUserToGroup(group_id, user_id)
             .then((groups) => {
               res.json(groups);
             })
