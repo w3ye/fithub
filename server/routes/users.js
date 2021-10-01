2fc28ea1231bd5a1e8f7b45c6520b939c0d43d35
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-10-01 16:39:18 -0400

    route is working for profile pic

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,6 +18,7 @@ module.exports = ({
   addUser,
   updateRefreshToken,
   getUserById,
+  updateProfilePic,
 }) => {
   /* GET users listing. */
   router.get("/", (req, res) => {
@@ -111,10 +112,10 @@ module.exports = ({
     return res.send({ accessToken });
   });
 
-  router.post("/user_image", (req, res) => {
+  router.post("/user_image", async (req, res) => {
     const { url, id } = req.body;
     updateProfilePic(url, id);
-    return res.send({ msg: Success });
+    return res.send({ msg: "Success" });
   });
 
   return router;
