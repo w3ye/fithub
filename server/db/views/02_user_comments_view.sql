223541a79ea1a91aff814e7c5cba97bab8ce12fe
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-02 12:17:50 -0400

    add user_comments and user_likes views

---
+++ server/db/views/02_user_comments_view.sql
@@ -0,0 +1,12 @@
+CREATE VIEW user_comments AS
+SELECT comments.id,
+  comments.user_id,
+  users.first_name,
+  users.last_name,
+  comments.workout_id,
+  workouts.title,
+  workouts.user_id AS owner_id,
+  comments.message
+FROM comments
+  JOIN workouts ON comments.workout_id = workouts.id
+  JOIN users ON users.id = comments.user_id;
