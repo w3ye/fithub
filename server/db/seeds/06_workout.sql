3a199977a97b669adbdf9b6adfa8b528004b1d92
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-02 11:38:05 -0400

    add seeds to workout, comments and likes

---
+++ server/db/seeds/06_workout.sql
@@ -0,0 +1,41 @@
+INSERT INTO workouts (title, user_id, exercises)
+VALUES (
+    'Beach body',
+    3,
+    ARRAY [ 
+    '{ "bodyPart": "back",
+    "equipment": "cable",
+    "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0007.gif",
+    "id": "0007",
+    "name": "alternate lateral pulldown",
+    "target": "lats" }' ]::jsonb []
+  ),
+  (
+    'Jump training',
+    1,
+    ARRAY ['
+  {
+    "bodyPart": "upper legs",
+    "equipment": "band",
+    "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0987.gif",
+    "id": "0987",
+    "name": "band one arm single leg split squat",
+    "target": "quads"
+  }',
+  '{
+    "bodyPart": "upper legs",
+    "equipment": "band",
+    "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/1001.gif",
+    "id": "1001",
+    "name": "band single leg split squat",
+    "target": "quads"
+  }',
+  '{
+    "bodyPart": "upper legs",
+    "equipment": "band",
+    "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/1004.gif",
+    "id": "1004",
+    "name": "band squat",
+    "target": "glutes"
+  }']::jsonb []
+  )
---
+++ server/db/seeds/09_comments.sql
@@ -0,0 +1,7 @@
+INSERT INTO comments (user_id, workout_id, message)
+VALUES (
+    2,
+    2,
+    'Great workout I hope I could jump as high as you one day'
+  ),
+  (4, 1, 'Summer season !!!!')
