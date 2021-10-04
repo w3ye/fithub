54924da2f916b63f735744ffa2ed9a60c8945dcc
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-04 14:28:25 -0400

    refactor topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
 import { TokenUserContext } from "../App/App";
 import "./topbar.scss";
-import { MdNotifications } from "react-icons/md";
 import axios from "axios";
 import User from "./User";
 
 export default function Topbar(props) {
-  const { setMain, request } = props;
+  const { setMain } = props;
 
   const { tokenState, userState } = useContext(TokenUserContext);
   const [token, setToken] = tokenState;
-  const [user, setUser] = userState;
+  const [setUser] = userState;
 
   async function logout() {
     axios
@@ -41,40 +40,9 @@ export default function Topbar(props) {
       <div className="topbarLeft">
         <span className="logo">FitHub</span>
       </div>
-      {/* <div className="topbarCenter">
-        <div className="searchBar">
-          <MdSearch className="searchIcon" />
-          <input
-            placeholder="Search for friend, post or video"
-            type="text"
-            className="searchInput"
-          />
-        </div>
-      </div> */}
       <div className="topbarRight">
-        {/* <div className="topbarLinks"> */}
-        {/* <span className='topbarLink'>Homepage</span>
-          <span className='topbarLink'>Workouts</span> */}
-        {/* <p>
-          Logged in as {user.user ? user.user.first_name + " " : ""}
-          {user.user ? user.user.last_name : ""}
-        </p> */}
-        {/* </div> */}
-        {/* <div className="topbarIcons"> */}
-        {/* <div className="topbarIconItem">
-          <MdPerson />
-        </div>
-        <div className="tobarIconItem">
-          <MdChat />
-        </div> */}
-        {/* {request.length && (
-          <div className="tobarIconItem">
-            <MdNotifications />
-          </div>
-        )} */}
         <User logout={logout} />
       </div>
-      {/* </div> */}
     </div>
   );
 }
