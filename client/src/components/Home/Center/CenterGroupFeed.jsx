a11e65d6560b418caf6bde9084b022762c36a547
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-04 13:13:01 -0400

    refactor center group feed

--- client/src/components/Home/Center/CenterGroupFeed.jsx
+++ client/src/components/Home/Center/CenterGroupFeed.jsx
@@ -1,24 +1,15 @@
 import "./center.scss";
-import Share from "./Share";
 import Posts from "./Posts";
-import GroupButton from "./GroupButton";
-import { TokenUserContext } from "../../App/App";
-import { useContext, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function CenterGroupFeed(props) {
-  const { userState } = useContext(TokenUserContext);
-  const [user] = userState;
-  const { setGroup, group } = props;
+  const { group } = props;
   const [groupData, setGroupData] = useState("");
 
   useEffect(() => console.log(""), [groupData]);
 
   useEffect(() => {
-    getWorkoutIds();
-  }, [group]);
-
-  function getWorkoutIds() {
     if (group.group_id) {
       axios
         .get(`/api/posts/${group.group_id}`)
@@ -29,7 +20,7 @@ export default function CenterGroupFeed(props) {
           return err;
         });
     }
-  }
+  }, [group]);
 
   const parsedWorkoutId =
     groupData &&
